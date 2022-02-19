26323d35166d15419f31e441b4d79e529f697ec6
Danila Kokin <danil.kokin231101@gmail.com>
2022-02-19 18:57:49 +0300

    master

---
+++ src/routes.js
@@ -0,0 +1,6 @@
+// export const authRoutes = [
+//     {
+//         path: '',
+//         Component: Ad
+//     }
+// ]
---
+++ src/utils/consts.js
@@ -0,0 +1,7 @@
+export const ADMIN_ROUTE = '/admin'
+export const COURSES_ROUTE = '/courses'
+export const SCHEDULE_ROUTE = '/schedule'
+export const ERROR_ROUTE = '/error'
+export const SETTINGS_ROUTE = '/settings'
+export const GRADES_ROUTE = '/grades'
+
